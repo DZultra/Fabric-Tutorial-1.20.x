eecf7a84ae6888247363043196cf4383f60537cd
dzultra <DanielZ123@web.de>
2023-11-19 13:22:47 +0100

    Added metal_detector_detectable_blocks Tag

--- src/main/java/net/dzultra/tutorialmod/item/custom/MetalDetectorItem.java
+++ src/main/java/net/dzultra/tutorialmod/item/custom/MetalDetectorItem.java
@@ -1,5 +1,6 @@
 package net.dzultra.tutorialmod.item.custom;
 
+import net.dzultra.tutorialmod.util.ModTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
@@ -51,7 +52,7 @@ public class MetalDetectorItem extends Item {
     }
 
     private boolean isValuableBlock(BlockState state) {
-        return state.isOf(Blocks.IRON_ORE) || state.isOf(Blocks.DEEPSLATE_IRON_ORE) || state.isOf(Blocks.GOLD_ORE) || state.isOf(Blocks.DEEPSLATE_GOLD_ORE);
+        return state.isIn(ModTags.Blocks.METAL_DETECTOR_DETECTABLE_BLOCKS);
     }
 
     @Override
